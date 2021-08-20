267b8db21bbc54959c5ee055d083cc4a653d5656
tomashoffer <tomashoffer96@gmail.com>
2021-08-20 11:06:34 +0300

    xc

---
+++ controlers/userControler.ts
@@ -0,0 +1,8 @@
+export {};
+const express = require("express");
+const app = express();
+
+
+export function logIn(req: any, res: any) {
+    
+}
---
+++ routes/userRoutes.ts
@@ -0,0 +1,8 @@
+export {};
+const express = require("express");
+const router = express.Router();
+
+// import { logInUser, logOutUser } from "../controllers/logInControllers";
+
+
+module.exports = router;
